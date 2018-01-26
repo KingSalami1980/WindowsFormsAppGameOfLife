a440740975091bcca5a7a2311d429c1700c13010
KingSalami1980 <35668864+KingSalami1980@users.noreply.github.com>
2018-01-26 21:44:11 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGameOfLife
+{
+    public partial class Form1 : Form
+    {
+        Board board = null;
+        Button start = null;
+        Button stop = null;
+        Label lblSteps = null;
+        TextBox steps = null;
+        int count = 32;
+        int squareSize = 15;
+        int topMargin = 40;
+
+        Thread runner = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int winWidth = (count + 1) * (squareSize + 1);
+            this.Width = winWidth;
+            int titleHeight = this.Height - this.ClientRectangle.Height;
+            this.Height = count * (squareSize + 1) + topMargin + titleHeight;
+            this.BackColor = Color.DarkGray;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board = new Board(count, squareSize, topMargin);
+            for (int i = 0; i < count * count; i++)
+            {
+                this.Controls.Add(board.square(i).panel());
+            }
+
+            start = new Button();
+            start.Text = "Start";
+            start.Height = topMargin;
+            start.Width = 100;
+            start.Click += Start_Click;
+            this.Controls.Add(start);
+
+            stop = new Button();
+            stop.Text = "Stop";
+            stop.Height = topMargin;
+            stop.Location = new Point(100, 0);
+            stop.Width = 100;
+            stop.Click += Stop_Click;
+            this.Controls.Add(stop);
+
+            lblSteps = new Label();
+            lblSteps.Height = topMargin;
+            lblSteps.Location = new Point(200, 0);
+            lblSteps.Width = 100;
+            lblSteps.Text = "Number of steps:";
+            this.Controls.Add(lblSteps);
+
+            steps = new TextBox();
+            steps.Height = topMargin;
+            steps.Location = new Point(300, 0);
+            steps.Width = 100;
+            steps.Text = "42";
+            this.Controls.Add(steps);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (runner == null || !runner.IsAlive)
+            {
+                runner = new Thread(board.Run);
+                if (steps.Text == "")
+                {
+                    MessageBox.Show("Number of steps is empty");
+                }
+                else
+                {
+                    int s;
+                    if (Int32.TryParse(steps.Text, out s))
+                    {
+                        runner.Start(s);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Number of steps must only contain an integer!");
+                    }
+                }
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (runner != null && runner.IsAlive)
+            {
+                runner.Abort();
+            }
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGameOfLife
+{
+    class Square
+    {
+        private Panel m_Panel;
+        private bool m_alive;
+        private bool m_NextState;
+
+        public Square(Point location, int size)
+        {
+            // Default state. Not alive
+            m_alive = false;
+            m_NextState = false;
+
+            // Setting up graphical representation
+            m_Panel = new Panel();
+            m_Panel.Height = size;
+            m_Panel.Width = size;
+            m_Panel.Location = location;
+            m_Panel.BackColor = Color.White;
+            m_Panel.Click += new System.EventHandler(Square_Click);
+        }
+
+        private void toggle()
+        {
+            if (m_alive)
+            {
+                this.m_Panel.BackColor = Color.White;
+            }
+            else
+            {
+                this.m_Panel.BackColor = Color.Black;
+            }
+
+            m_alive = !m_alive;
+        }
+
+        private void update()
+        {
+            if (m_alive)
+            {
+                this.m_Panel.BackColor = Color.Black;
+            }
+            else
+            {
+                this.m_Panel.BackColor = Color.White;
+            }
+        }
+
+        private void Square_Click(object sender, EventArgs e)
+        {
+            toggle();
+        }
+
+        public void updateDisplay()
+        {
+            m_alive = m_NextState;
+            update();
+        }
+
+        public Panel panel()
+        {
+            return this.m_Panel;
+        }
+
+        public bool isAlive()
+        {
+            return m_alive;
+        }
+
+        public void setState(bool state)
+        {
+            m_NextState = state;
+        }
+
+        public void setState()
+        {
+            m_NextState = m_alive;
+        }
+    }
+}
